129db4b7b2c52ae6ddc7283d19e781a0e1867851
woong <dndvud123@naver.com>
2022-10-19 23:51:00 +0900

    make user models and seralizers

--- market/models.py
+++ market/models.py
@@ -1,3 +1,7 @@
+from enum import unique
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=20)
