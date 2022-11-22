8ec3f5853c33b074b4556d2c8697bb503f87fb3a
woong <dndvud123@naver.com>
2022-11-22 23:58:16 +0900

    토큰 재발급

--- market/urls.py
+++ market/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import HelloAPI, RegisterAPIView, Authview
 
 urlpatterns = [
     path('hello/', HelloAPI),
     path("register/", RegisterAPIView.as_view()), #회원가입하기
     path("auth/", Authview.as_view()), #로그인하기
+    path("auth/refresh/", TokenRefreshView.as_view()), #토큰 재발급하기
 ]
