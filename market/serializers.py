f5b63e3ba06bf78adfa6c7cbe48320db68234b58
woong <dndvud123@naver.com>
2023-02-03 21:20:05 +0900

    ProfileSerializer 추가

--- market/serializers.py
+++ market/serializers.py
@@ -1,28 +1,44 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from .models import User, Post
+from .models import User, Post, Profile
 
 
 User = get_user_model()
-class RegisterSerializer(serializers.ModelSerializer):
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = [
+            'realname',
+            'username',
+            'prfile_pic',
+            'provie',
+        ]
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
     class Meta:
         model = User
-        fields = '__all__'
+        fields = [
+            'nickname',
+            'email',
+            'profile',
+        ]
+
+class RegisterSerializer(serializers.Serializer):
+    first_name = serializers.CharField(max_length=150, write_only=True)
+    last_name = serializers.CharField(max_length=150, write_only=True)
+    email = serializers.EmailField(write_only=True)
+    nickname = serializers.CharField(max_length=150, write_only=True)
+    password = serializers.CharField(write_only=True)
         
     def create(self, validated_data):
         user = User.objects.create_user(  # type: ignore
-            nickname = validated_data['nickname'],
-            email = validated_data['email'],
-            password = validated_data['password']
+            validated_data
         )
         
         return user
-    
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
-        
+
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
